0b159fc3b1cc31dd70f3d43e9cc6c3d155aa1a51
Jaquim Cadogan <jaquimcadogan@gmail.com>
2016-01-07 14:50:20 +0100

    worked and planned hours function rearrange

--- l1nda.py
+++ l1nda.py
@@ -15,23 +15,38 @@ warnings.filterwarnings("ignore")
 
 pd.options.display.max_colwidth = 100
 
-file_name = 'COMPANY_37_BRANCH_141'
-input_path = './datadump/' + file_name + '.csv'
-output_path = './datadump/' + file_name + '_OUTPUT.csv'
 
-data = pd.read_csv(input_path)
+def fetch_data(file_name='COMPANY_37_BRANCH_141'):
+  
+    input_path = './datadump/' + file_name + '.csv'
 
-data_last_planned = data[data['is_last_planned'] == 't']
+    output_worked_path = './datadump/' + file_name + '_WORKED_OUTPUT.csv'
+    output_planned_path = './datadump/' + file_name + '_PLANNED_OUTPUT.csv'
 
-start = pd.to_datetime(data_last_planned['start'])
-end = pd.to_datetime(data_last_planned['end'])
+    data = pd.read_csv(input_path)
 
-data_last_planned['date'] = start.dt.strftime('%Y-%m-%d')
-data_last_planned['start'] = start.dt.strftime('%H:%M:%S')
-data_last_planned['end'] = end.dt.strftime('%H:%M:%S')
-data_last_planned['hours'] = (end-start)
-data_last_planned.to_csv(output_path, sep=',')
-print(data_last_planned.head())
+    data_planned = data[data['is_last_planned'] == 't']
+    data_worked = data[(data['is_deleted'] == 'f') | (data['is_deleted'] == 't') \
+                & (data['forward_id'] == data['event_version_id'])]
+
+    data_worked = transform_data(data_worked, output_worked_path)
+    data_planned = transform_data(data_planned, output_planned_path)
+
+
+def transform_data(data_frame, output_path):
+
+    start = pd.to_datetime(data_frame['start'])
+    end = pd.to_datetime(data_frame['end'])
+
+    data_frame['date'] = start.dt.strftime('%Y-%m-%d')
+    data_frame['start'] = start.dt.strftime('%H:%M:%S')
+    data_frame['end'] = end.dt.strftime('%H:%M:%S')
+    data_frame['hours'] = (end-start)
+
+    data_frame.to_csv(output_path, sep=',')
+
+    print(data_frame.head())
+    return data_frame
 
 
 # read in data set from the features file.
