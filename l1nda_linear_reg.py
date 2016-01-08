4672802f419278fbbef29d358d7d4a9cb386b863
Jaquim Cadogan <jaquimcadogan@gmail.com>
2016-01-08 13:18:58 +0100

    functionalize for data dict

--- l1nda_linear_reg.py
+++ l1nda_linear_reg.py
@@ -1,18 +1,24 @@
+#!/usr/bin/python
+
 import numpy as np
 import pandas as pd
 #import statsmodels.api as sm
 import l1nda
 from scipy.stats import pearsonr
 
+data_worked, data_planned = l1nda.fetch_data()
+
 
 #Compute the correlation for two numpy arrays
 def compute_correlation(X, Y):
     correlation_vector = list()
     correlation_vector.append(pearsonr(np.ravel(X.tolist()), np.ravel(Y.tolist()))[0])
-    print correlation_vector
+    return correlation_vector
 
 
-data_worked, data_planned = l1nda.fetch_data()
-for layer_name, data_frame in data_worked.items():
-	print  layer_name
-	compute_correlation(data_frame[0], data_frame[1])
+def compute_layer_correlation(data_dict):
+    for layer_name, data_frame in data_dict.items():
+        layer_correlation = compute_correlation(data_frame['data'], data_frame['y_vector'])
+        print(layer_correlation)
+
+compute_layer_correlation(data_worked)
