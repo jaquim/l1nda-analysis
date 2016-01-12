a91feb67bbc91f37d9ad4ab903e69b08b12d3a07
Wout Kooijman <wapkooijman@gmail.com>
2016-01-12 15:13:13 +0100

    updated prediction.py

--- prediction.py
+++ prediction.py
@@ -2,8 +2,8 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 
-layer = 'layer928'
-company_branch = 'COMPANY_45_BRANCH_325'
+layer = 'layer977'
+company_branch = 'COMPANY_50_BRANCH_340'
 
 
 def read_data_log_reg(file_name, sep):
@@ -14,7 +14,7 @@ def read_data_log_reg(file_name, sep):
 def calc_pred(data_worked, data_planned, data_date):
     prediction_list, hours_list = list(), list()
     for row in data_worked.iterrows():
-        prediction = 0.8755 * row[1]['mean_weekday_lastyear'] + 0.1273 * row[1]['last_week_worked_hours'] - 0.1166
+        prediction = 16.1251 * row[1]['festivity'] + 1.0374 * row[1]['weather_grades'] + 0.4132 * row[1]['mean_weekday_lastyear'] + 0.4991 * row[1]['last_week_worked_hours'] + 0.1518 * row[1]['last_year_worked_hours'] - 9.1044
         prediction_list.append(prediction)
         hours_list.append(row[1]['hours'])
     planned_list, date_list = list(), list()
@@ -33,7 +33,8 @@ def plot_results_real(prediction_list, hours_list, planned_list, date_list, file
     plt.plot(prediction_list, label='prediction')
     plt.plot(hours_list, label='real hours')
     plt.xticks(range(len(date_list)), date_list)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.03),
+          ncol=3, fancybox=True, shadow=True)
     plt.savefig('./../L1nda_plots/' + file_name + '_hours.png')
 
 def plot_results_difference(prediction_list, hours_list, planned_list, date_list, file_name):
@@ -50,7 +51,8 @@ def plot_results_difference(prediction_list, hours_list, planned_list, date_list
     plt.axhline(0)
     plt.plot(diff_planner, label='planner')
     plt.xticks(range(len(date_list)), date_list)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.03),
+          ncol=3, fancybox=True, shadow=True)
     plt.savefig('./../L1nda_plots/' + file_name + '_difference.png')
 
 
