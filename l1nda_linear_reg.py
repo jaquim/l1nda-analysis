4f96443efb4a9b655553bc0d665ed2ba3bb91177
Jaquim Cadogan <jaquimcadogan@gmail.com>
2016-01-18 16:18:22 +0100

    made max code work

--- l1nda_linear_reg.py
+++ l1nda_linear_reg.py
@@ -42,9 +42,9 @@ def compute_layer_correlation(data_dict, features, company_affiliate_name):
         print('Mean correlation for %s:\n %s\n' % (company_affiliate_name, annotated_correlation))
 
 
-def info(coef_list, data_planned, data_worked, info_dir):
+def info(data_planned, data_worked, info_dir, coef_list, total_frame):
 
-    prediction_list, worked_list, planned_list, date_list = prediction.calc_pred(coef_list, data_worked, data_planned)
+    prediction_list, worked_list, planned_list, date_list = prediction.calc_pred(data_worked, data_planned, coef_list)
 
     # Prediction
 
@@ -82,7 +82,7 @@ def info(coef_list, data_planned, data_worked, info_dir):
     counter = 0
     over_planned_planner = 0
     under_planned_planner = 0
-    for planned, worked in zip(prediction_list, worked_list):
+    for planned, worked in zip(planned_list, worked_list):
         hours_wrong = planned-worked
 
         if hours_wrong > 0:
@@ -98,7 +98,7 @@ def info(coef_list, data_planned, data_worked, info_dir):
     total = 0
     counter = 0
 
-    for planned, worked in zip(prediction_list, worked_list):
+    for planned, worked in zip(planned_list, worked_list):
         hours_wrong = abs(planned-worked)
         total = total + abs(hours_wrong - mean_planner)
         counter += 1
@@ -107,7 +107,7 @@ def info(coef_list, data_planned, data_worked, info_dir):
 
     percentage = total_pred/total_planned * 100
 
-    info = pd.dataFrame()
+    info = pd.DataFrame(index=range(1))
 
     info['mean_missplanned_planner'] = mean_planner
     info['mean_missplanned_prediction'] = mean_pred
@@ -117,11 +117,13 @@ def info(coef_list, data_planned, data_worked, info_dir):
     info['under_planned_planner'] = under_planned_planner
     info['over_planned_pred'] = over_planned_pred
     info['under_planned_pred'] = under_planned_pred
-    info['coef_list'] = coef_list
+    # info['coef_list'] = coef_list
     info['percentage'] = percentage
-    info['most_predicting_feature'] = max(coef_list, key=lambda x: x[1])
+    info['most_predicting_feature'] = max(coef_list, key=lambda x: x[1])[0]
 
-    layer_name = info_dir + '_info'
+    total_frame = total_frame.append(info)
+
+    layer_name = info_dir + '_overview.csv'
 
     info.to_csv(layer_name, sep=',', index=False)
 
@@ -129,27 +131,32 @@ def info(coef_list, data_planned, data_worked, info_dir):
 def create_linear_models():
     json_dir = './datadump/json/'
     results_dir = './datadump/results/'
+    total_frame = pd.DataFrame()
     if os.path.exists(results_dir):
                 shutil.rmtree(results_dir)
     os.mkdir(results_dir)
 
     for json_file in os.listdir(json_dir):
-
+        print(json_file)
         info_dir = os.path.join(results_dir, os.path.splitext(json_file)[0])
-        if os.path.exists(info_dir):
-                shutil.rmtree(info_dir)
-        os.mkdir(info_dir)
+
+        if not os.path.exists(info_dir):
+                os.mkdir(info_dir)
 
         with open(os.path.join(json_dir, json_file)) as data:
             json_data = json.load(data)
 
             for schedule_type, schedule in json_data.items():
                 if schedule_type == 'WORKED':
+                    print(len(schedule.items()))
                     for layer, data_frame in schedule.items():
+                        print(layer)
                         json_data[schedule_type][layer] = pd.read_json(data_frame)
                         exclude = ['date', 'hours']
                         data_frame = pd.read_json(data_frame)
+
                         data_frame = data_frame[(data_frame['date'] > '2014-12-31')]
+
                         X = data_frame.ix[:, data_frame.columns.difference(exclude)]
                         y = data_frame['hours']
                         est = sm.OLS(y, X).fit()
@@ -158,12 +165,13 @@ def create_linear_models():
 
                         data_planned = pd.read_json(json_data['PLANNED'][layer])
 
-                        layer_name = info_dir + '/' + layer
+                        layer_name = info_dir + '/' + layer + '/'
 
-                        prediction.predict(data_frame, data_planned, coef_list, layer_name)
-                        info(coef_list, data_planned, data_frame, layer_name)
-                        break
-                    break
-                break
-            break
+                        if not os.path.exists(layer_name):
+                            os.mkdir(layer_name)
+
+                        prediction.predict(data_frame, data_planned, coef_list, layer_name + layer)
+                        info(data_planned, data_frame, layer_name + layer, coef_list, total_frame)
+    print(total_frame)
+    print(total_frame.describe())
 create_linear_models()
