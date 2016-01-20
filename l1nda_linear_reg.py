7e73f23bc19f0402ad61bf9a2ef6d99e4f51e72d
Jaquim Cadogan <jaquimcadogan@gmail.com>
2016-01-20 22:27:25 +0100

    cleanse comment

--- l1nda_linear_reg.py
+++ l1nda_linear_reg.py
@@ -112,9 +112,6 @@ def info(prediction_list, worked_list, planned_list, layer_name, coef_list, coef
     info['most_predicting_feature'] = max(coef_list, key=lambda x: x[1])[0]
     info['model'] = str(coef_model)
 
-    # wegschrijven per branch?
-    # make a list of done things to get it back from where it stopped (log)
-
     # Add the info to a dataframe of the info of all the layers, for future use
     total_frame = total_frame.append(info)
 
